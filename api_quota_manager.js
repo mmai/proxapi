2032f5cd76d03d2d21d53e99fea06e2bd743fda9
Henri Bourcereau <henri.bourcereau@gmail.com>
2014-09-04 21:12:26 +0200

    tests : mock usage limited api

--- api_quota_manager.js
+++ api_quota_manager.js
@@ -6,15 +6,20 @@ ApiQuotaManager = function(params){
   this.strategy = params.strategy;
 };
 
-ApiQuotaManager.prototype.call = function(params){
+ApiQuotaManager.prototype.call = function(params, iter){
   var self = this;
   var deferred = Q.defer();
   this.api_call(params, function(error, quota, data){
       if (error){
         deferred.reject(error);
       }
-      console.log("on resolve...");
-      deferred.resolve(null, self.api_callback(error, data));
+      if (quota){
+        setTimeout(function(){
+            deferred.resolve(self.call(params, iter));
+          }, 5000);
+      } else {
+        deferred.resolve(self.api_callback(error, data));
+      }
   });
   return deferred.promise;
 };
--- test/api_quota_manager.spec.js
+++ test/api_quota_manager.spec.js
@@ -1,15 +1,32 @@
 var chai = require("chai");
 var expect = chai.expect;
 
+var Q = require('q');
 var AQM = require("../api_quota_manager.js");
 
 describe('ApiQuotaManager', function(){
 
     //A public API mock
+    var limit = 5;
+    var call_count = 0;
+
+    function resetCount(){
+      call_count = 0;
+      setTimeout(resetCount, 5000);
+    }
+    resetCount();
+
     var mockapi = function(nom, prenom, callback){
       var error = null;
-      var data = 'hello ' + nom +" "+prenom;
       var response = "200 OK";
+      var data = "";
+      if (call_count++ > limit){
+        error = "Limit reached";
+        response = "500";
+      } else {
+        // console.log(call_count);
+        data = 'hello ' + nom +" "+prenom;
+      }
       callback(error, data, response);
     };
 
@@ -17,7 +34,8 @@ describe('ApiQuotaManager', function(){
     var mockapi_call = function(params, proxy_callback){
       mockapi(params.nom, params.prenom, function(error, data, response){
           var quota = false;
-          if (response == "500"){
+          if (response == "500" || error == "Limit reached"){
+            error = null;
             quota = true;
           }
           proxy_callback(error, quota, data);
@@ -26,9 +44,7 @@ describe('ApiQuotaManager', function(){
 
     var manage_results = function(err, data){
       if (err) {
-        console.log('Error : ' + err);
       } else {
-        console.log("==== " +  data + " ======");
       }
       return data;
     };
@@ -47,18 +63,31 @@ describe('ApiQuotaManager', function(){
       });
 
     describe('call()', function(){
+        this.timeout(30000);
         it('should call the api whith all parameters', function(done){
             var params = {nom: "john", prenom:"doe" };
 
             mockapi(params.nom, params.prenom, function(error, data, response){
-                api_proxy.call(params).then(function(err, res){
+                api_proxy.call(params).then(function(res){
                     expect(res).to.equal(data);
                     done();
-                  });
+                  }).done();
               });
           });
-      });
-  });
 
+        it('should retry', function(done){
+          var params = {nom: "john", prenom:"doe" };
+          var promises = [];
 
-
+          for (var i=0;i<15;i++){
+            promises.push(api_proxy.call(params));
+          }
+          Q.all(promises).then(function(results){
+              expect(true);
+              done();
+            }).fail(function(err){
+                console.log(err)
+              }).done();
+        });
+    });
+});
